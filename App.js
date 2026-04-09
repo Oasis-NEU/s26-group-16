885f70ff8016ed981f702c7ebedcf36df4574354
ZahiL <ladkhan.z@northeastern.edu>
2026-04-08 23:00:32 -0400

    connect supabase backend with auth

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 // import function from homescreen
 import HomeScreen from './src/screens/HomeScreen'
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import TabNavigator from './src/navigation/TabNav'
+import { useState, useEffect } from 'react'
+import { supabase } from './src/lib/supabase'
+import AuthScreen from './src/screens/AuthScreen'
 
 // main function/entry point to start app's frontend
 // will be called on expo's servers
 export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
     // Container for main navigation (i.e the bottom tab)
     <NavigationContainer>
-      <TabNavigator/>
+      {/* Shows authentication screen on first login */}
+      {session ? <TabNavigator /> : <AuthScreen />}
     </NavigationContainer>
   )
 }
